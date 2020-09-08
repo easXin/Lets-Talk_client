d26da24c617ae3b8e96e83da6147bed2a9f26c40
easXin <codingericwang@gmail.com>
2020-09-08 00:25:22 -0700

    if backend cookie is not exist then push user to login page

--- src/containers/main/main.jsx
+++ src/containers/main/main.jsx
@@ -3,39 +3,22 @@ import {Switch, Route,Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import Recruiter from '../recruiter-page/recruiter-page';
 import Candidate from '../candidate-page/candidate-page';
-import {redirectUPage} from '../../utils/index';
+
 import Cookies from 'js-cookie';
 class Main extends Component {
-  // initial render, constructor, componentWillMount, render, componentDidMount,componentWillUnmount
-  componentDidMount() {
-    const userid = Cookies.get('userid')
-    const {user} = this.props
-    if (userid && !user._id) {
- //
-    }
-  }
+ //  componentDidMount() {
+ //    const userid = Cookies.get('userid')
+ //    const {user} = this.props
+ //    if (userid && !user._id) {
+ // //
+ //    }
+ //  }
   // if backend cookie is not exist then push user to login page
   render() {
-    // if the user already login, then backend should sent me cookie id
-    const cookie = Cookies.get('userid');
-    // if there is no cookie, then push user back to the login page
-    if(!cookie) {
+    const cookies = Cookies.get('userid')
+    const {user} = this.props
+    if (!cookies||!user._id) { // 如果没值, 自动跳转到登陆界面
       return <Redirect to='/login'/>
-    }
-    // if user just log in, then backend should sent me a _id in the props
-    const {user} = this.props;   //  res.cookie('userid',user._id,{maxAge:1000*60*7});
-    const curPath = this.props.location.pathname;
-    if(!user._id && curPath!=='/'){
-      return <Redirect to='/login'/> // bug?
-    }else{
-      const path = redirectUPage(user.type, user.avatar)
-      return <Redirect to={path}/>
-      // const userType = this.props.user.type;
-      // if(userType.toLowerCase() === 'candidate'){
-      //   return <Redirect to='/candidate'/>
-      // }else{
-      //   return <Redirect to='/recruiter'/>
-      // }
     }
 
     return (
